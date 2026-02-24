1ff6d6c43a3b1be8e4027e8135a564b9cffaf765
woodrowb <brenwoodr96@gmail.com>
2026-02-24 15:33:00 -0800

    refactor: cleanup tb_top code formatting and switch to blocking assignments for clk gen

--- uvm/tb_top.sv
+++ uvm/tb_top.sv
@@ -1,10 +1,5 @@
 /*
-This file is the top level module for the testbench
-
-This file 
-	Defines an interface, to connect DUT to Test
-	Connects Test to DUT through interface
-	Starts test
+The top level module for our uvm testbench
 */
 
 import tb_tests::*;
@@ -15,31 +10,35 @@ import uvm_pkg::*;
 
 module tb_top;
 
-	//define clock
-	logic clk;
-	always #(CLK_PERIOD * 0.5) clk <= ~clk;
+  /********** CLK ************/
+  logic clk;
+  initial begin
+    clk = 0;
+    forever #(CLK_PERIOD / 2) clk = ~clk;
+  end
 
-	//interface
-	tb_fifo_intf _if(clk);	
+  /******** INTERFACE ********/
+  tb_fifo_intf _if(clk);
 
-	//connect DUT to interface
-	fifo #(LENGTH,WIDTH,ADDR_WIDTH) dut (
-		.clk(clk),
-		.reset_n(_if.reset_n),
-		.wr_data(_if.wr_data),
-		.wr(_if.wr),
-		.rd(_if.rd),
-		.rd_data(_if.rd_data),
-		.item_count(_if.item_count),
-		.full(_if.full),
-		.empty(_if.empty),
-		.underflow(_if.underflow),
-		.overflow(_if.overflow)
-	);
+  /******** DUT *************/
+  fifo #(LENGTH, WIDTH, ADDR_WIDTH) dut (
+    .clk(clk),
+    .reset_n(_if.reset_n),
+    .wr_data(_if.wr_data),
+    .wr(_if.wr),
+    .rd(_if.rd),
+    .rd_data(_if.rd_data),
+    .item_count(_if.item_count),
+    .full(_if.full),
+    .empty(_if.empty),
+    .underflow(_if.underflow),
+    .overflow(_if.overflow)
+  );
 
-	initial begin
-		clk <= 0;
-		uvm_config_db#(virtual tb_fifo_intf)::set(null,"uvm_test_top","des_vif",_if);	//connect interface to test
-		run_test("test_1");							//start test
-	end
+  /*********** UVM TESTING ************/
+  //hook the interface up to uvm_test and run
+  initial begin
+    uvm_config_db#(virtual tb_fifo_intf)::set(null,"uvm_test_top","des_vif",_if);
+    run_test("test_1");
+  end
 endmodule
